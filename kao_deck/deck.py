437dff6e7fae5d2f6d9d481b30b2d0fef4ba6b52
Chris Loew <cloew123@gmail.com>
2013-08-17 13:31:45 -0600

    Updated some variables to be a little more specific

--- kao_deck/deck.py
+++ kao_deck/deck.py
@@ -10,19 +10,19 @@ class Deck:
             self.__contents__ = deck_initializer.contents()
         else:
             raise TypeError("No Item List or Deck Initializer provided")
-        self.__discard_pile__ = []
+        self.__disitem_pile__ = []
       
     def draw(self, count=1):
-        """ Returns a list of cards removed from the top of the deck """
-        cards = []
+        """ Returns a list of items removed from the top of the deck """
+        items = []
         for i in range(count):
-            card = self.__draw_one__()
-            if card is not None:
-                cards.append(card)
-        return cards
+            item = self.__draw_one__()
+            if item is not None:
+                items.append(item)
+        return items
         
     def __draw_one__(self):
-        """ Draws a single card """
+        """ Draws a single item """
         if len(self.deck) == 0:
             return None
         return self.deck.pop()
