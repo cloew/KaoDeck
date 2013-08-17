e54ddb72e5ff00c922b42071304fc341a85f655d
Chris Loew <cloew123@gmail.com>
2013-08-17 10:38:27 -0600

    Added setup file to install the module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kao_deck',
+      version='1.0',
+      #description='Kao Tessur Deck Package',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['kao_deck'],
+     )
